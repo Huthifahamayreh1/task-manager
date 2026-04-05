dff204664a0d9143617ddc2981be30644594bfa7
Huthifahamayreh1 <7o7o.a.amay@gmail.com>
2026-04-05 19:02:46 +0300

    task 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,19 +3,23 @@ import { TaskList } from './pages/task-list/task-list';
 import { TaskDetails } from './pages/task-details/task-details';
 import { TaskForm } from './pages/task-form/task-form';
 
+import { BlankLayout } from './layout/blank-layout/blank-layout';
+import { MainLayout } from './layout/main-layout/main-layout';
+
 export const routes: Routes = [
     {
-        path:"",
-        component:TaskList
+        path: "",
+        component: MainLayout,
+        children: [{ path: "", component: TaskList }]
     },
     {
-        path:"task/:id",
-        component:TaskDetails
+        path: 'add-task',
+        component: BlankLayout,
+        children: [{ path: "", component: TaskForm }]
     },
     {
-        path:"add-task",
-        component:TaskForm
-
-
+        path: 'task/:id',
+        component: BlankLayout,
+        children: [{ path: "", component: TaskDetails }]
     }
 ];
---
+++ src/app/pages/header-component/header-component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLinkWithHref } from '@angular/router';
+
+@Component({
+  selector: 'app-header-component',
+  imports: [RouterLinkWithHref],
+  templateUrl: './header-component.html',
+  styleUrl: './header-component.css',
+})
+export class HeaderComponent {}
